650d32e7a5076bd728659a7e790cf7cfff733299
acandael <info@anthonycandaele.com>
2019-11-15 14:01:24 +0100

    implemented firebase utils, included ability to store authenticated users

--- src/App.js
+++ src/App.js
@@ -14,34 +14,47 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render () {
+  render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser} />
-       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-       </Switch>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,20 +4,38 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDAdaMcr026JSCv0N22DPLstePvmJAvYKQ",
-  authDomain: "crown-db-783cc.firebaseapp.com",
-  databaseURL: "https://crown-db-783cc.firebaseio.com",
-  projectId: "crown-db-783cc",
-  storageBucket: "crown-db-783cc.appspot.com",
-  messagingSenderId: "598453127623",
-  appId: "1:598453127623:web:6957d59359c6deabdb31e2"
+  apiKey: 'AIzaSyDAdaMcr026JSCv0N22DPLstePvmJAvYKQ',
+  authDomain: 'crown-db-783cc.firebaseapp.com',
+  databaseURL: 'https://crown-db-783cc.firebaseio.com',
+  projectId: 'crown-db-783cc',
+  storageBucket: 'crown-db-783cc.appspot.com',
+  messagingSenderId: '598453127623',
+  appId: '1:598453127623:web:6957d59359c6deabdb31e2'
 };
 
-oxport const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  console.log(firestore.doc('users/128fdashadu'))
-}
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+};
 
 firebase.initializeApp(config);
 
@@ -25,7 +43,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account' });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
